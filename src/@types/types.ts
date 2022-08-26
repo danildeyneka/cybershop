89002381d1b8204fa5468d433ae89cffec1fd5be
danil deyneka <danildeyneka@gmail.com>
2022-08-27 00:22:18 +0500

    init

---
+++ src/@types/types.ts
@@ -0,0 +1,10 @@
+export type DatabaseType = {
+    id: number
+    category: 'mouse' | 'keyboard' | 'headphones'
+    brand: string
+    name: string
+    oldPrice: null | number
+    price: number
+    photo: string
+    desc: string
+}
